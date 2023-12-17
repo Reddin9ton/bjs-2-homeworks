9cde27f84bbd120f685aacb1394afe7675f0909c
red <artikus256@gmail.com>
2023-12-17 18:43:43 +0300

    code dz 1 edited

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discr = (b**2) - 4 * a * c;
+	let arr = [];
+	let discr = (b ** 2) - 4 * a * c;
 
-  if (discr === 0) {
-   let rootOneForm = -b / (2 * a);
-    arr = [rootOneForm];
+	if (discr === 0) {
+		let rootOneForm = -b / (2 * a);
+		arr = [rootOneForm];
 
-  }
+	}
 
-  if (discr > 0) {
-   let rootOneForm = (-b + Math.sqrt(discr) )/(2*a);
-   let rootTwoForm = (-b - Math.sqrt(discr) )/(2*a);
-    arr = [rootOneForm, rootTwoForm];
-  }
+	if (discr > 0) {
+		let rootOneForm = (-b + Math.sqrt(discr)) / (2 * a);
+		let rootTwoForm = (-b - Math.sqrt(discr)) / (2 * a);
+		arr = [rootOneForm, rootTwoForm];
+	}
 
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = (percent / 100) / 12;
-  let S = amount - contribution;
-  let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
-  return Number((payment * countMonths).toFixed(2));
+	let P = (percent / 100) / 12;
+	let S = amount - contribution;
+	let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+	return Number((payment * countMonths).toFixed(2));
 
 }
