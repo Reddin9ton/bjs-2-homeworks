6f6a5936bc2abb65f5ee61f72595f17e82b5bb3a
red <artikus256@gmail.com>
2024-01-29 22:34:49 +0300

    edited classes homework

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -4,24 +4,103 @@ class PrintEditionItem {
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
         this.state = 100;
+        this._state = 100;
         this.type = null;
     }
     fix() {
         this.state *= 1.5;
-        this.setter = this.state;
     }
-    set setter(newState) {
+    set state(newState) {
         if(newState < 0) {
-            this.state = 0;
+            this._state = 0;
         } else if(newState > 100) {
-            this.state = 100;
+            this._state = 100;
     
         }else {
-            this.state = newState;
+            this._state = newState;
         }
         }
-    get getter() {
-       return this.state;
+    get state() {
+       return this._state;
     }
 }
 
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "novel";
+
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "fantastic";
+
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "detective";
+
+    }
+}
+
+class Library {
+    constructor(name, books = []) {
+        this.name = name;
+        this.books = books;
+
+    }
+    addBook(book) {
+        if(book.state > 30) {
+            this.books.push(book);
+        }
+    }
+    findBookBy(type, value) {
+        for (const book of this.books) {
+            if(book[type] === value) {
+                return book;
+            } else {
+                return null;
+            }
+        }
+    }
+    giveBookByName(bookName) {
+        for (const book of this.books) {
+            if (book.name === bookName) {
+                const index = this.books.indexOf(book);
+                if (index !== -1) {
+                    this.books.splice(index, 1);
+                    return book;
+                }
+            }
+        }
+        return null;
+    }
+    
+    
+}        
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,17 @@
-﻿
+﻿function parseCount(count) {
+    parse = Number.parseFloat(count);
+    if (isNaN(parse)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return parse;
+    }
+}
+
+function validateCount(count) {
+    try {
+        return parseCount(count);
+    } catch (error) {
+        return error;
+    }
+
+}
