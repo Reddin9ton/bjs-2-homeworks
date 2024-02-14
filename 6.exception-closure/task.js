f32e78e5bf76c61233979675a07e42eede45b59d
red <artikus256@gmail.com>
2024-02-14 21:55:48 +0300

    edit dz

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -17,45 +17,38 @@ function validateCount(count) {
 }
 
 class Triangle {
-    constructor(sideOne,sideTwo,sideThree) {
+    constructor(sideOne, sideTwo, sideThree) {
         this.sideOne = sideOne;
         this.sideTwo = sideTwo;
         this.sideThree = sideThree;
-        this.perimeter = 0;
-        this._perimeter = 0;
-        this.area = 0;
-        this._area = 0;
 
-        if (sideOne > (sideTwo + sideThree) || sideTwo > (sideOne + sideThree) || sideThree > (sideOne + sideTwo)) {
+        if (sideOne >= (sideTwo + sideThree) || sideTwo >= (sideOne + sideThree) || sideThree >= (sideOne + sideTwo)) {
             throw new Error("Треугольник с такими сторонами не существует");
         }
- }
-
- set perimeter(perimeter) {
-    perimeter = this.sideOne + this.sideTwo + this.sideThree;
-    this._perimeter = perimeter;
- }
- get perimeter() {
-    return this._perimeter;
- }
- set area(area) {
-    const p = this._perimeter / 2;
-    area = Number(Math.sqrt(p * (p - this.sideOne) * (p - this.sideTwo) * (p - this.sideThree)).toFixed(3));
-    this._area = area;
- }
- get area() {
-    return this._area;
-}
-}
-
-function getTriangle(sideOne,sideTwo,sideThree) {
-    try {
-        const myObject = new Triangle(sideOne,sideTwo,sideThree);
-        return myObject;
-    } catch (error) {
-        return {
-        perimeter: () => "Ошибка! Треугольник не существует",
-        area: () => "Ошибка! Треугольник не существует",
+        
     }
+
+ get perimeter() {
+    const perimeter = this.sideOne + this.sideTwo + this.sideThree;
+    return perimeter;
+ }
+
+ get area() {
+    const perimeter = this.perimeter;
+    const p = perimeter / 2;
+    let area = Number(Math.sqrt(p * (p - this.sideOne) * (p - this.sideTwo) * (p - this.sideThree)).toFixed(3));
+    return area;
+}
+}
+
+function getTriangle(sideOne, sideTwo, sideThree) {
+    try {
+        const myObject = new Triangle(sideOne, sideTwo, sideThree);
+        return myObject;
+    } catch (e) {
+        return {
+            perimeter: () => 'Ошибка! Треугольник не существует',
+            area: () => 'Ошибка! Треугольник не существует',
+        };
     }
 }
